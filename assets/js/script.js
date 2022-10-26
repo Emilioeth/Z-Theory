e600ee0541a5bb134461aa860e5fa6f20a444e67
Emilio <emiliothomasking@gmail.com>
2022-10-25 22:29:35 -0500

    changed preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,26 @@
 // Page loading animation
-$(window).on('load', function() {
-    if($('.cover').length){
-        $('.cover').parallax({
-            imageSrc: $('.cover').data('image'),
-            zIndex: '1'
-        });
-    }
 
-    $("#preloader").animate({
-        'opacity': '0'
-    }, 600, function(){
-        setTimeout(function(){
-            $("#preloader").css("visibility", "hidden").fadeOut();
-        }, 300);
-    });
-});
+let loadInterval = setInterval(() => {
+    if (document.readyState === 'complete') {
+        console.log('1')
+        clearInterval(loadInterval)
+        if($('.cover').length){
+            $('.cover').parallax({
+                imageSrc: $('.cover').data('image'),
+                zIndex: '1'
+            });
+        }
+    
+        $("#preloader").animate({
+            'opacity': '0'
+        }, 600, function(){
+            setTimeout(function(){
+                $("#preloader").css("visibility", "hidden").fadeOut();
+            }, 300);
+        });
+    };
+}, 500);
+
 
 
 const mainContainer = document.querySelector('.projects-container');
