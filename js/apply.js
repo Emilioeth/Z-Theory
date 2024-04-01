842126590ab0e739f8ef2596d1a1b2b71d05b3c8
Emili0 <emiliothomasking@gmail.com>
2024-04-01 16:07:41 -0500

    ahhh

--- js/apply.js
+++ js/apply.js
@@ -1,4 +1,22 @@
 
+console.log('hello')
+
+function ValidateEmail(input) {
+
+    var validRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+    if (input.match(validRegex)) {
+  
+      return true;
+  
+    } else {
+  
+      return false;
+  
+    }
+  
+}
+
 function formActivation() {
 
     setTimeout(() => {
@@ -64,19 +82,3 @@ function ApplyForm() {
     
 
 }//End ApplyForm	
-
-function ValidateEmail(input) {
-
-    var validRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  
-    if (input.match(validRegex)) {
-  
-      return true;
-  
-    } else {
-  
-      return false;
-  
-    }
-  
-}
